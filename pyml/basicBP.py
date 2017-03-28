bf8d0138086f03993e9f408e2f5b805143a8c3fd
winn <iamzwz3@gmail.com>
2017-03-28 15:24:17 +0800

    add basic bpnet mainframe

---
+++ pyml/basicBP.py
@@ -0,0 +1,16 @@
+class BPNet(object):
+    def __init__(self):
+    def logistic(self, net):
+    def dlogit(self, net):
+    def errorfunc(self, inX):
+    def normalize(self, dataMat):
+    def loadDataSet(self, filename):
+    def addcol(self, matrix1, matrix2):    
+    def init_hiddenWB(self):
+    def bpTrain(self):
+    def BPClassfier(self, start, end, steps=30):
+    def classfyLine(self, plt, x, z):
+    def TrendLine(self, plt, color='r'):
+    def drawClassScatter(self, plt):
+
+
