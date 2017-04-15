b658f6110e4a3ba482b828140a38bd86040835df
winn <iamzwz3@gmail.com>
2017-04-15 21:10:59 +0800

    update kmeans

--- firstcontact/kmeans.py
+++ firstcontact/kmeans.py
@@ -11,16 +11,21 @@ for i in range(num_puntos):
         conjunto_puntos.append([np.random.normal(0.0, 0.9), np.random.normal(0.0, 0.9)])
     else:
         conjunto_puntos.append([np.random.normal(3.0, 0.5), np.random.normal(1.0, 0.5)])
+#conjunto_puntos = [[x0,y0],[x1,y1],...,[x1999,y1999]]
 
-df = pd.DataFrame({"x": [v[0] for v in conjunto_puntos], "y": [v[1] for v in conjunto_puntos]})
-sns.lmplot("x", "y", data= df, fit_reg = False, size=6)
-plt.show()
+#df = pd.DataFrame({"x": [v[0] for v in conjunto_puntos], "y": [v[1] for v in conjunto_puntos]})
+#sns.lmplot("x", "y", data= df, fit_reg = False, size=6)
+#plt.show()
 
 vectors = tf.constant(conjunto_puntos)
-k = 4
+#print(vectors.get_shape()) -> [2000,2]
+k = 6 
 centroides = tf.Variable(tf.slice(tf.random_shuffle(vectors),[0,0],[k,-1]))
+#print(centroides.get_shape()) -> [4,2]
 expanded_vectors = tf.expand_dims(vectors, 0)
 expanded_centroides = tf.expand_dims(centroides, 1)
+#print(expanded_vectors.get_shape()) -> [1, 2000, 2]
+#print(expanded_centroides.get_shape()) -> [4, 1, 2]
 
 assignments = tf.argmin(tf.reduce_sum(tf.square(tf.sub(expanded_vectors, expanded_centroides)), 2), 0)
 means = tf.concat(0, [tf.reduce_mean(tf.gather(vectors, tf.reshape(tf.where(tf.equal(assignments, c)), [1,-1])), reduction_indices = [1]) for c in range(k)])
