ea922efb107c3fa2bdd66c9f405f458e3a9fb7a7
winn <iamzwz3@gmail.com>
2017-04-20 21:23:39 +0800

    add lr mnist

---
+++ firstcontact/lr_mnist.py
@@ -0,0 +1,49 @@
+import numpy as np
+import tensorflow as tf
+import input_data
+import matplotlib.pyplot as plt
+
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+trainimg = mnist.train.images
+trainlabel = mnist.train.labels
+testing = mnist.test.images
+testlabel = mnist.test.labels
+
+x = tf.placeholder("float", [None, 784])
+y = tf.placeholder("float", [None, 10])
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+actv = tf.nn.softmax(tf.matmul(x,W) +b)
+cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(actv), reduction_indices=1))
+
+learning_rate = 0.01
+optm = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+pred = tf.equal(tf.argmax(actv, 1), tf.argmax(y, 1))
+accr = tf.reduce_mean(tf.cast(pred, "float"))
+
+init = tf.initialize_all_variables()
+
+training_epochs = 50
+batch_size = 100
+display_step = 5
+
+sess = tf.Session()
+sess.run(init)
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    num_batch = int(mnist.train.num_examples/batch_size)
+    for i in range(num_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        sess.run(optm, feed_dict={x: batch_xs, y: batch_ys})
+        feeds = {x:batch_xs, y: batch_ys}
+        avg_cost += sess.run(cost, feed_dict=feeds)/num_batch
+    
+    if epoch % display_step == 0:
+        feeds_train = {x: batch_xs, y: batch_ys}
+        feeds_test = {x: mnist.test.images, y: mnist.test.labels}
+        train_acc = sess.run(accr, feed_dict=feeds_train)
+        test_acc = sess.run(accr, feed_dict=feeds_test)
+        print("Epoch: %03d/%03d cost: %.9f train_acc: %.3f test_acc: %.3f" % (epoch, training_epochs, avg_cost, train_acc, test_acc))
